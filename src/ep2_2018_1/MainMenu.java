6a8755b750427d393a005284554f14d450ab5917
Lucas Dutra Ferreira do Nascimento <ldutra98@gmail.com>
2018-05-17 23:48:43 -0300

    Adicionando as funcionalidades pelo WindowBuilder e interface de menu pronta

--- src/ep2_2018_1/MainMenu.java
+++ src/ep2_2018_1/MainMenu.java
@@ -1,43 +1,66 @@
 package ep2_2018_1;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
 
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.BoxLayout;
+import javax.swing.SwingConstants;
+import java.awt.Component;
 
-public class MainMenu extends JFrame{
-	
-	private JButton Play;
-	private JButton Instructions;
-	private JButton Infos;
-	private JLabel Rodape;
+public class MainMenu extends JFrame {
 
-	public MainMenu() {
-		
-		super("Batalha Naval!");
-		
-		Play = new JButton("JOGAR");
-		Instructions = new JButton("INSTRUÇÕES");
-		Infos = new JButton("INFORMAÇÕES");
-		
-		setVisible(true);
-		setLayout(new BoxLayout(parent, EXIT_ON_CLOSE));
-		setSize(800,800);
-		
-		Play.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				// TODO Auto-generated method stub
-				
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainMenu frame = new MainMenu();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 			}
 		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MainMenu() {
 		
-		Play.setAlignmentX(CENTER_ALIGNMENT);
-		Instructions.setAlignmentX(CENTER_ALIGNMENT);
-		Infos.setAlignmentX(CENTER_ALIGNMENT);
-	 }
-	 
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 519, 444);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnJogar = new JButton("JOGAR");
+		btnJogar.setBounds(197, 79, 107, 23);
+		contentPane.add(btnJogar);
+		
+		JButton btnInstructions = new JButton("INSTRU\u00C7\u00D5ES");
+		btnInstructions.setBounds(197, 175, 107, 23);
+		contentPane.add(btnInstructions);
+		
+		JButton btnInfos = new JButton("INFORMA\u00C7\u00D5ES");
+		btnInfos.setBounds(197, 278, 107, 23);
+		btnInfos.setAlignmentX(2.0f);
+		btnInfos.setAlignmentY(2.0f);
+		contentPane.add(btnInfos);
+		
+		JLabel footer = new JLabel("Lucas Dutra 17/0050939");
+		footer.setBounds(10, 380, 127, 14);
+		contentPane.add(footer);
+		
+	}
 }
