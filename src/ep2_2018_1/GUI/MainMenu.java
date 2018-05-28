9cfa80b5262ff1d2dda711b2cfad678ad590ecec
Lucas Dutra Ferreira do Nascimento <ldutra98@gmail.com>
2018-05-28 00:32:54 -0300

    Adding image to the main menu

--- src/ep2_2018_1/GUI/MainMenu.java
+++ src/ep2_2018_1/GUI/MainMenu.java
@@ -11,6 +11,8 @@ import javax.swing.JLabel;
 import javax.swing.BoxLayout;
 import javax.swing.SwingConstants;
 import java.awt.Component;
+import javax.swing.ImageIcon;
+import java.awt.Color;
 
 public class MainMenu extends JFrame {
 
@@ -36,24 +38,27 @@ public class MainMenu extends JFrame {
 	 * Create the frame.
 	 */
 	public MainMenu() {
+		setForeground(Color.CYAN);
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 854, 589);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(127, 255, 212));
+		contentPane.setForeground(Color.BLACK);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JButton btnPlay = new JButton("JOGAR");
-		btnPlay.setBounds(344, 112, 137, 23);
+		btnPlay.setBounds(324, 281, 137, 23);
 		contentPane.add(btnPlay);
 		
 		JButton btnInstructions = new JButton("INSTRU\u00C7\u00D5ES");
-		btnInstructions.setBounds(344, 216, 137, 23);
+		btnInstructions.setBounds(324, 361, 137, 23);
 		contentPane.add(btnInstructions);
 		
-		JButton btnInfos = new JButton("INFORMA\u00C7\u00D5ES");
-		btnInfos.setBounds(344, 318, 137, 23);
+		JButton btnInfos = new JButton("PLACARES");
+		btnInfos.setBounds(324, 445, 137, 23);
 		btnInfos.setAlignmentX(2.0f);
 		btnInfos.setAlignmentY(2.0f);
 		contentPane.add(btnInfos);
@@ -61,6 +66,11 @@ public class MainMenu extends JFrame {
 		JLabel footer = new JLabel("Lucas Dutra 17/0050939");
 		footer.setBounds(10, 525, 183, 14);
 		contentPane.add(footer);
+		
+		JLabel lblTheme = new JLabel("New label");
+		lblTheme.setIcon(new ImageIcon(MainMenu.class.getResource("/ep2_2018_1/images/Theme.png")));
+		lblTheme.setBounds(115, 35, 585, 191);
+		contentPane.add(lblTheme);
 		
 	}
 }
